f974da9a65a3ddf022bd095ba41ae8d7b95a7c92
hyukjunkim1116 <nmkk1234@naver.com>
2024-04-13 14:19:05 +0900

    1. variable

---
+++ README.md
@@ -0,0 +1,6 @@
+자바 기본기 다지기
+
+IntelliJ 단축키,단축어
+
+1. psvm : public static void main(String[] args) 단축어
+2. sout : System.out.println() 단축어
---
+++ src/main/java/beginner/variable/VarEx3.java
@@ -0,0 +1,9 @@
+package beginner.variable;
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVal = 10000000000L;
+        boolean boolVal = true;
+        System.out.println(longVal);
+        System.out.println(boolVal);
+    }
+}
